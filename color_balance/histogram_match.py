ce37e92a7ef955f7bbf0b1189e5cdd7f72550b87
Amit Kapadia <amit@planet.com>
2015-07-08 16:53:12 -0700

    notes on current implementation of cdf match

--- color_balance/histogram_match.py
+++ color_balance/histogram_match.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright 2014 Planet Labs, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
 
 import logging
 import numpy as np
@@ -45,6 +45,8 @@ def cdf_normalization_luts(in_img, ref_img, in_mask=None, ref_mask=None):
         rband = ref_img[:, :, bidx]
 
         in_cdf = ci.get_cdf(iband, mask=in_mask)
+
+        # TODO: ref_mask blocks entire image.
         ref_cdf = ci.get_cdf(rband, mask=ref_mask)
         lut = cdf_match_lut(in_cdf, ref_cdf)
         out_luts.append(lut)
@@ -52,51 +54,72 @@ def cdf_normalization_luts(in_img, ref_img, in_mask=None, ref_mask=None):
     return out_luts
 
 
-def cdf_match_lut(in_cdf, match_cdf):
-    '''Create a look up table for matching the input cdf to the match cdf. At
-    each intensity, this algorithm gets the value of the input cdf, then finds
-    the intensity at which the match cdf has the same value.'''
+def cdf_match_lut(src_cdf, ref_cdf, dtype=np.uint8):
+    """
+    Create a look up table for matching the source CDF to the reference CDF. At
+    each intensity, this algorithm gets the value of the source CDF, then finds
+    the intensity at which the reference CDF has the same value.
+    
+    :param src_cdf:
+        Source CDF represented as a 1d array.
+    :param ref_cdf:
+        Reference CDF represented as a 1d array.
+    :param dtype:
+        Numpy data type used to represent the output LUT
+    :return look up table:
+    """
 
-    _check_cdf(in_cdf)
-    _check_cdf(match_cdf)
-    assert len(in_cdf) == len(match_cdf), \
-        "cdfs don't have same number of entries"
+    _check_cdf(src_cdf)
+    _check_cdf(ref_cdf)
+    
+    # TODO: Why enforce the lengths to be the same? Needed for current approach,
+    #       but limiting algo to fixed-bin histograms.
+    assert len(src_cdf) == len(ref_cdf), \
+        "CDFs don't have same number of entries"
 
     # This approach is preferred over using
-    # np.interp(in_cdf, match_cdf, range(len(in_cdf))), which
+    # np.interp(src_cdf, ref_cdf, range(len(src_cdf))), which
     # stretches the intensity values to min/max available intensities
     # even when matching CDF doesn't have entries at min/max intensities
     # (confirmed by unit tests)
-    lut = np.arange(len(in_cdf), dtype=np.int)
-    for i, c_val in enumerate(in_cdf):
-        match_i = np.searchsorted(match_cdf, c_val)
-        lut[i] = match_i
+    lut = np.zeros_like(src_cdf, dtype=dtype)
+    
+    # TODO: What's going on here? Why not use np.take and save the python for loop?
+    for i, val in enumerate(src_cdf):
 
-    # Clip to max/min values of band, as determined from cdf
+        # TODO: Is this O(n^2)?
+        lut[i] = np.searchsorted(ref_cdf, val)
+
+    # Clip to max/min values of band, as determined from CDF
     # This is necessary because np.searchsorted maps a value
     # to either 0 or len(array) if it doesn't find a target location
-    max_value = np.argmax(match_cdf == match_cdf.max())
-    min_value = np.argmax(match_cdf > 0)
+    max_value = np.argmax(ref_cdf == ref_cdf.max())
+    min_value = np.argmax(ref_cdf > 0)
 
     logging.info("clipping lut to [{},{}]".format(min_value, max_value))
     np.clip(lut, min_value, max_value, lut)
 
     if np.any(np.diff(lut) < 0):
-        raise Exception('cdf_match lut not monotonically increasing')
+        raise Exception('ref_cdf lut not monotonically increasing')
 
-    return lut.astype(np.uint8)
+    return lut
 
 
-def _check_cdf(test_cdf):
-    '''Checks that CDF monotonically increases and has a maximum value of 1'''
-    if np.any(np.diff(test_cdf) < 0):
+def _check_cdf(cdf):
+    """
+    Checks that CDF monotonically increases and has a maximum value of 1.
+    
+    .. todo:: Is this check necessary? Will np.cumsum() ever return a non-monotonically increasing function?
+    """
+
+    if np.any(np.diff(cdf) < 0):
         raise CDFException('not monotonically increasing')
 
-    if abs(test_cdf[-1] - 1.0) * 10**10 > 1:
-        raise CDFException('maximum value {} not close enough to 1.0'.format(test_cdf[-1]))
+    if abs(cdf[-1] - 1.0) * 10**10 > 1:
+        raise CDFException('maximum value {} not close enough to 1.0'.format(cdf[-1]))
 
-    if test_cdf[0] < 0:
-        raise CDFException('minimum value {} less than 0'.format(test_cdf[0]))
+    if cdf[0] < 0:
+        raise CDFException('minimum value {} less than 0'.format(cdf[0]))
 
 
 def mean_std_luts(in_img, ref_img, in_mask=None, ref_mask=None):
